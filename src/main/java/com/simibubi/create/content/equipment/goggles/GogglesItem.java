f576bee3df96f4708989b9db6bb3c51e995d2a3d
myname <lharr@protonmail.com>
2026-02-09 21:43:11 +0100

    Fix goggles head-slot equipping on Fabric

--- src/main/java/com/simibubi/create/content/equipment/goggles/GogglesItem.java
+++ src/main/java/com/simibubi/create/content/equipment/goggles/GogglesItem.java
@@ -7,18 +7,18 @@ import java.util.function.Predicate;
 import com.simibubi.create.AllItems;
 
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.Equipable;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DispenserBlock;
 
-public class GogglesItem extends Item {
+public class GogglesItem extends Item implements Equipable {
 
 	private static final List<Predicate<Player>> IS_WEARING_PREDICATES = new ArrayList<>();
 	static {
@@ -35,17 +35,14 @@ public class GogglesItem extends Item {
 		return EquipmentSlot.HEAD;
 	}
 
+	@Override
+	public EquipmentSlot getEquipmentSlot() {
+		return EquipmentSlot.HEAD;
+	}
+
+	@Override
 	public InteractionResultHolder<ItemStack> use(Level worldIn, Player playerIn, InteractionHand handIn) {
-		ItemStack itemstack = playerIn.getItemInHand(handIn);
-		EquipmentSlot equipmentslottype = playerIn.getEquipmentSlotForItem(itemstack);
-		ItemStack itemstack1 = playerIn.getItemBySlot(equipmentslottype);
-		if (itemstack1.isEmpty()) {
-			playerIn.setItemSlot(equipmentslottype, itemstack.copy());
-			itemstack.setCount(0);
-			return new InteractionResultHolder<>(InteractionResult.SUCCESS, itemstack);
-		} else {
-			return new InteractionResultHolder<>(InteractionResult.FAIL, itemstack);
-		}
+		return this.swapWithEquipmentSlot(this, worldIn, playerIn, handIn);
 	}
 
 	public static boolean isWearingGoggles(Player player) {
